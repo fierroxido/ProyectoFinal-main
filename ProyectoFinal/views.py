cbf67a0d2115b1e7b470d025b21a7e4cd45083d5
fierroxido <101884944+fierroxido@users.noreply.github.com>
2022-09-27 21:59:57 -0500

    a
    a

--- ProyectoFinal/views.py
+++ ProyectoFinal/views.py
@@ -20,8 +20,8 @@ def servicios(request):
     return render(request, 'servicios/servicios.html');
 
 def registro(request):
-    context={}
-    if request.method=='POST':
+   # context={}
+    """if request.method=='POST':
         rol=request.POST['rol']
         first_name=request.POST['first_name']
         last_name=request.POST['last_name']
@@ -46,43 +46,43 @@ def registro(request):
         context['alarma']='Las contraseñas no coinciden'
 #Ok
 
-if ok:
-    existe=Account.objects.filter(email=email).exists()
-    print('---------')
-    print(existe)
+    if ok:
+        existe=Account.objects.filter(email=email).exists()
+        print('---------')
+        print(existe)
 
-    if not existe:
-        user=Account.objects.create_user(first_name=username,last_name=username,username=username,email=email,password=password)
-        user.save()
-        context['mensaje']='Usuario guardado con exito'
+        if not existe:
+            user=Account.objects.create_user(first_name=first_name,last_name=first_name,username=first_name,email=email,password=password)
+            user.save()
+            context['mensaje']='Usuario guardado con exito'
 
-#Modulo para envío de correo
+    #Modulo para envío de correo
 
-        current_site=get_current_site(request)
-        mail_subject='Por favor activa tu cuenta'
+            current_site=get_current_site(request)
+            mail_subject='Por favor activa tu cuenta'
 
-        body=render_to_string(¿usuarios/account_verification_email.html',{
-            'user':user,
-            'domain':current_site,
-            'uid':str(urlsafe_base64encode(force_bytes(user.pk)))
-            'token':default_token_generator.make_token(user),
-        })
-        to_email=email
-        send_email=EmailMessage(mail_subject,body, to=[to_email])
-        send_email.send()
-        context={'mensaje':'Bienvenido'+first_name+'.Favor activar su cuenta con el enlace que enviamos a su correo'}
+            body=render_to_string(¿usuarios/account_verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':str(urlsafe_base64encode(force_bytes(user.pk)))
+                'token':default_token_generator.make_token(user),
+            })
+            to_email=email
+            send_email=EmailMessage(mail_subject,body, to=[to_email])
+            send_email.send()
+            context={'mensaje':'Bienvenido'+first_name+'.Favor activar su cuenta con el enlace que enviamos a su correo'}
 
-        return redirect('accounts/login/')
+            return redirect('accounts/login/')
 
-    else:
-        context['alarma']= '!El correo ya existe!'
+        else:
+            context['alarma']= '!El correo ya existe!'
 
-return render(request, 'registro.html',context);#Y
-
-
-
-        #--- return render(request, 'registro.html'); Us
+    return render(request, 'registro.html',context);#Y
 
 
 
+            #--- return render(request, 'registro.html'); Us
+
+"""
+    return render(request, 'registro.html');
 
